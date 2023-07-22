b5931f1ad8d9c2f3ca3ab0ea8bdb9ffdfbad016e
phonty29 <amedov.bekmuhamet@gmail.com>
2023-07-22 15:06:30 +0600

    feat:os-3ep:intro: memo.c

---
+++ os-3ep/code/intro/memo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "common.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: mem<value>\n");
+        exit(1);
+    }
+
+    int *p;
+    p = malloc(sizeof(int));
+    assert(p != NULL);
+    printf("(%d) addr pointed  to by p: %p\n", (int) getpid(), p);
+    *p = atoi(argv[1]); //assign value to address stored in p
+    while (1) {
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d) value of p: %d\n", (int) getpid(), *p);
+    }
+    return 0;
+}
