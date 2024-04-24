c3cb01864f7afa9b8ff6aef162182b9c7e4aaff0
phonty29 <amedov.bekmuhamet@gmail.com>
2024-04-24 22:41:04 +0500

    chore: cleanup repo

--- algs-p1-princeton/hello/HelloGoodbye.java
+++
@@ -1,8 +0,0 @@
-public class HelloGoodbye {
-    public static void main(String[] args) {
-        String fName= args[0];
-        String sName=args[1];
-        System.out.println(String.format("Hello %s and %s", fName, sName));
-        System.out.println(String.format("Goodbye %s and %s", sName, fName));
-    }
-}
